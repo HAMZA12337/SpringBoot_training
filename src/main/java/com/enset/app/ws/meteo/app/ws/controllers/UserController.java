3ab6811c9462187599dd968da6b0a737126f883a
Your Name <you@example.com>
2023-01-19 01:02:42 +0100

    Spring MVC

--- src/main/java/com/enset/app/ws/meteo/app/ws/controllers/UserController.java
+++ src/main/java/com/enset/app/ws/meteo/app/ws/controllers/UserController.java
@@ -1,5 +1,41 @@
+// name space
 package com.enset.app.ws.meteo.app.ws.controllers;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// rest controller called innotation 
+@RestController
+@RequestMapping("/users") //equivalent localhost/users 
 public class UserController {
 
+	
+	
+	@GetMapping
+	public String getUser() {
+		
+	return "get user was called " ;}
+	
+	@PostMapping
+	public String createUser() {
+		
+		return "Create user was called " ;}
+	
+	@PutMapping
+	public String updateUser() {
+		
+	return "update user was called ";}
+	
+	@DeleteMapping
+	public String deleteUser() {
+		
+	return "delete user was called ";	
+	}
+	
+	
+	
 }
