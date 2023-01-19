fdb97a8742d8bba6c8472e64eeb083baf6c9a69d
Your Name <you@example.com>
2023-01-19 13:43:34 +0100

    mvc architect

---
+++ src/main/java/com/enset/app/ws/meteo/app/services/UserService.java
@@ -0,0 +1,5 @@
+package com.enset.app.ws.meteo.app.services;
+
+public class UserService {
+
+}
--- src/main/java/com/enset/app/ws/meteo/app/ws/controllers/UserController.java
+++ src/main/java/com/enset/app/ws/meteo/app/ws/controllers/UserController.java
@@ -1,12 +1,19 @@
 // name space
 package com.enset.app.ws.meteo.app.ws.controllers;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.enset.app.ws.meteo.app.shared.dto.UserDto;
+import com.enset.app.ws.meteo.app.ws.requests.UserRequest;
+import com.enset.app.ws.meteo.app.ws.responses.UserResponse;
+
 
 // rest controller called innotation 
 @RestController
@@ -21,9 +28,22 @@ public class UserController {
 	return "get user was called " ;}
 	
 	@PostMapping
-	public String createUser() {
+	public UserResponse createUser(@RequestBody UserRequest userRequest  ) {
 		
-		return "Create user was called " ;}
+		
+	 UserDto userDto= new UserDto();
+     
+	 // to create user 
+	 // we create this object to return just some attributes(ni password) of dto class
+	 BeanUtils.copyProperties(userRequest, userDto);
+     UserDto createUser =userService.createuser(userDto);
+	 //to create response
+     UserResponse userResponse= new UserResponse();
+	 BeanUtils.copyProperties(createUser, userResponse);
+	
+	 return userResponse;
+	
+	}
 	
 	@PutMapping
 	public String updateUser() {
@@ -36,6 +56,7 @@ public class UserController {
 	return "delete user was called ";	
 	}
 	
-	
+	//pascalecase: UserController
+	//canalcase   userController
 	
 }
